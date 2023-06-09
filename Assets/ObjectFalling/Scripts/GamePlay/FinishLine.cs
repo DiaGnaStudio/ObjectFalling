f624de5480a7e77608243be901c1c965b3412c0d
Usef Farahmand <50228817+UsefFarahmand@users.noreply.github.com>
2023-06-09 21:22:55 +0330

    change finish line displayer

--- Assets/ObjectFalling/Scripts/GamePlay/FinishLine.cs
+++ Assets/ObjectFalling/Scripts/GamePlay/FinishLine.cs
@@ -8,8 +8,10 @@ namespace DiaGna.ObjectFalling.Gameplay
     public class FinishLine : MonoBehaviour
     {
         [SerializeField] private float m_WinHight;
-
         private float m_CurrentHight;
+
+        [Header("Line")]
+        [SerializeField] private Material m_LineMaterial;
 
         /// <summary>
         /// Invoke when a brick fall down and ground rotating is down.
@@ -17,6 +19,11 @@ namespace DiaGna.ObjectFalling.Gameplay
         /// <b>Parameters:</b> returns true if the bricks hight reached to the win hight.
         /// </summary>
         public static event Action<bool> OnReached;
+
+        private void Awake()
+        {
+            CreateFinishLine();
+        }
 
         private void OnEnable()
         {
@@ -45,6 +52,17 @@ namespace DiaGna.ObjectFalling.Gameplay
             UnityEditor.Handles.color = Color.green;
             UnityEditor.Handles.DrawWireDisc(hightPosition, Vector3.up, 5);
 #endif
+        }
+
+        private void CreateFinishLine()
+        {
+            var line = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            line.transform.SetParent(transform);
+            line.transform.localPosition = new Vector3(3, m_WinHight/2, 0);
+            line.transform.localRotation = Quaternion.Euler(0, 45, 0);
+            line.transform.localScale = new Vector3(0.5f,m_WinHight,0.5f);
+
+            line.GetComponent<Renderer>().material = m_LineMaterial;
         }
     }
 }
