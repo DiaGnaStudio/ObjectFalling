41009f3450a12b5e842a12596c47ae733e7ceea6
Usef Farahmand <50228817+UsefFarahmand@users.noreply.github.com>
2023-06-10 22:53:14 +0330

    fix crane movement with joystick

---
+++ Assets/ObjectFalling/Scripts/UserInterface/Tools/MoverJoystick.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace DiaGna.ObjectFalling.UserInterface.Tools
+{
+    /// <summary>
+    /// An UserInterface tools to provides movement event by <see cref="Joystick"/>.
+    /// </summary>
+    [RequireComponent(typeof(Joystick))]
+    public class MoverJoystick : MonoBehaviour
+    {
+        private Joystick m_joystick;
+
+        public static event Action<Vector2> OnDrag;
+        public static event Action OnPointerUp;
+
+        private void Awake()
+        {
+            m_joystick = GetComponent<Joystick>();
+        }
+
+        private void OnEnable()
+        {
+            m_joystick.OnPointerUpEvent += OnPointerUp;
+        }
+
+        private void OnDisable()
+        {
+            m_joystick.OnPointerUpEvent -= OnPointerUp;
+        }
+
+        private void FixedUpdate()
+        {
+            MoveByJoystick();
+        }
+
+        private void MoveByJoystick()
+        {
+            OnDrag?.Invoke(new Vector2(m_joystick.Horizontal, 0));
+        }
+    }
+}
